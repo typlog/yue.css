efcaa695bef19b798b9770b3f7ec0d6ef8abda24
Hsiaoming Yang <me@lepture.com>
2013-12-21 00:18:24 +0800

    Generate html from readme

--- template.py
+++ template.py
@@ -10,8 +10,9 @@ with open('index.html', 'r') as f:
 with open('README.md', 'r') as f:
     text = f.read()
 
-text = '<body>\n<div class="yue">\n%s\n</div>\n</body>' % markdown(text)
+text = markdown(text.decode('utf-8'), highlight=False)
+text = '<body>\n<div class="yue">\n%s\n</div>\n</body>' % text
 html = re.sub(r'<body>.*</body>', text, template, flags=re.S)
 
 with open('index.html', 'w') as f:
-    f.write(html)
+    f.write(html.encode('utf-8'))
